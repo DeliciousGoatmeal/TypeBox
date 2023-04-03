fa5e96baf694ab6ec6046131cee430ce923a4a8c
John Thacher <jethacher@gmail.com>
2023-04-03 15:22:21 -0700

    Initial Commit

---
+++ TypeBox/TypeBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeBoxApp.swift
+//  TypeBox
+//
+//  Created by Goat on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TypeBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
